d3ab432cd756835a417c95d104d1dd0ba9128401
msteven <mark.stevens@virginmedia.co.uk>
2017-10-16 16:53:57 +0100

    Updated to compositeDisposable

--- app/src/main/java/com/alsk/onebyone/MainActivity.java
+++ app/src/main/java/com/alsk/onebyone/MainActivity.java
@@ -13,8 +13,6 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -23,15 +21,22 @@ import java.util.concurrent.Callable;
 import io.reactivex.Emitter;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int TOTAL_ELEMENTS_COUNT = 206560;
     private ActivityMainBinding binding;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    private Observable<Response<ResponseBody>> responseObservable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,27 +45,31 @@ public class MainActivity extends AppCompatActivity {
         playHugeJsonSample();
     }
 
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
+
     public void playHugeJsonSample() {
 
         HugeJsonApi hugeJsonApi = RestUtils.createService(HugeJsonApi.class, HugeJsonApi.SERVICE_ENDPOINT);
-
+        responseObservable = hugeJsonApi.get();
         final int[] counter = {0};
-        Gson gson = new GsonBuilder().create();
-
-        hugeJsonApi.get()
-                .flatMap(responseBody -> convertObjectsStream(responseBody, gson, Feature.class))
+        final Gson gson = new GsonBuilder().create();
+        binding.progressbar.setMax(TOTAL_ELEMENTS_COUNT);
+        binding.progressbar.setIndeterminate(false);
+        binding.progressbar.setProgress(0);
+        compositeDisposable.add(responseObservable
+                .flatMap(new Function<Response<ResponseBody>, Observable<Feature>>(){
+                    @Override
+                    public Observable<Feature> apply(@io.reactivex.annotations.NonNull Response<ResponseBody> response) throws Exception {
+                        return convertObjectsStream(response, gson, Feature.class);
+                    }
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Feature>() {
-
-                    @Override
-                    public void onSubscribe(Subscription s) {
-
-                        binding.progressbar.setMax(TOTAL_ELEMENTS_COUNT);
-                        binding.progressbar.setIndeterminate(false);
-                        binding.progressbar.setProgress(0);
-                        request(1);
-                    }
+                .subscribeWith(new DisposableObserver<Feature>() {
 
                     @Override
                     public void onNext(Feature feature) {
@@ -69,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
                         binding.tvLastElement.setText("Last read element: " + gson.toJson(feature));
                         binding.tvStatus.setText("Used memory: " + getUsedMemoryInMb() + "Mb");
                         binding.progressbar.setProgress(counter[0]);
-                        request(1);
                     }
 
                     @Override
@@ -81,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete() {
                         binding.tvStatus.setText("Status: successfully completed");
                     }
-                });
+                }));
     }
 
     private long getUsedMemoryInMb() {
@@ -92,14 +100,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @NonNull
-    private static <TYPE> Observable<TYPE> convertObjectsStream(ResponseBody responseBody, Gson gson, Class<TYPE> clazz) {
+    private static <TYPE> Observable<TYPE> convertObjectsStream(final Response<ResponseBody> response, Gson gson, Class<TYPE> clazz) {
         Type type = TypeToken.get(clazz).getType();
         return Observable.generate(new Callable<JsonReader>() {
 
             @Override
             public JsonReader call() throws Exception {
                 try {
-                    JsonReader reader = gson.newJsonReader(responseBody.charStream());
+                    JsonReader reader = gson.newJsonReader(response.body().charStream());
                     reader.beginObject();
                     // looking for a "features" field with actual array of elements
                     while (reader.hasNext()) {
